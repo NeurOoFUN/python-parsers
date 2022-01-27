941b410ee25d5b38a83fab1b34470643f93eba4a
NeurOoFUN <orel0858@gmail.com>
2022-01-27 17:50:34 +0300

    start develop auto_parser

--- avito_auto_parser/auto_parser/spiders/avto.py
+++ avito_auto_parser/auto_parser/spiders/avto.py
@@ -1,12 +1,21 @@
 import scrapy
+from scrapy.loader import ItemLoader
+
+from auto_parser.items import AutoParserItem
 
 
 class AvtoSpider(scrapy.Spider):
     name = 'avto'
     allowed_domains = ['avito.ru']
 
-    def start_requests(self, response):
-        pass
+    def start_requests(self):
+        start_urls = [
+            'https://www.avito.ru/uzlovaya/avtomobili?radius=200',
+        ]
+        for urls in start_urls:
+            yield scrapy.Request(url=urls, callback=self.parse)
 
     def parse(self, response):
-        pass
+        i = ItemLoader(item=AutoParserItem(), response=response)
+        i.add_value('url', response.url)
+        yield i
