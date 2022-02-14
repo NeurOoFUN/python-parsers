b710df73ae877bcec7147a2dc41a3d0680e58e88
NeurOoFUN <orel0858@gmail.com>
2022-02-14 19:38:04 +0300

    start dev proxy parser

--- proxy_parser/proxy/items.py
+++ proxy_parser/proxy/items.py
@@ -1,12 +1,8 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from scrapy.loader.processors import TakeFirst
 
 
 class ProxyItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    url = scrapy.Field()
+    ip = scrapy.Field()
+    port = scrapy.Field()
--- proxy_parser/proxy/spiders/proxy_spider.py
+++ proxy_parser/proxy/spiders/proxy_spider.py
@@ -1,12 +1,28 @@
-import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
+
+from proxy.items import ProxyItem
 
 
-class ProxySpider(scrapy.Spider):
+class ProxySpider(CrawlSpider):
     name = 'proxy_spider'
-    allowed_domains = ['proxy.com']
-
-    def start_requests(self):
-        pass
+    allowed_domains = ['hidemy.name']
+    start_urls = [
+        'https://hidemy.name/ru/proxy-list/'
+    ]
+    rules = (
+        Rule(
+            LinkExtractor(allow=('hidemy.name/ru/proxy-list/',),),
+            callback='parse',
+        ),
+    )
 
     def parse(self, response):
-        pass
+        i = ItemLoader(item=ProxyItem(), response=response)
+        i.add_value('url', response.url)
+        i.add_xpath(
+            'ip', '//div[@class="table_block"]//tbody/tr/td[1]//text()')
+        i.add_xpath(
+            'port', '//div[@class="table_block"]//tbody/tr/td[2]//text()')
+        yield i.load_item()
