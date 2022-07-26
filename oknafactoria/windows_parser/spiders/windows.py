f598187dacc6fe623c66acc52468b9dc32eb4c4f
NeurOo <orel0858@gmail.com>
2022-07-26 21:35:23 +0300

    add *.csv in .gitignore, delete selenium middleware

--- oknafactoria/windows_parser/spiders/windows.py
+++ oknafactoria/windows_parser/spiders/windows.py
@@ -4,14 +4,19 @@ from windows_parser.items import WindowsParserItem
 
 
 class WindowsSpider(scrapy.Spider):
+    start_urls = []
     name = 'windows'
-    start_urls = 'https://oknafactoria.ru/gotovie-okna/',
+    for number_page in range(21, 28):
+        start_urls.append(
+            f'https://oknafactoria.ru/gotovie-okna/page/{str(number_page)}/'
+        )
 
     def start_requests(self):
         for urls in self.start_urls:
             yield scrapy.Request(
-                url=urls, callback=self.parse_links_for_each_window
-            )
+                url=urls,
+                callback=self.parse_links_for_each_window
+                )
 
     def parse_links_for_each_window(self, response):
         links = response.xpath(
