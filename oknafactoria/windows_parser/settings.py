9f32300fd8bbecb080a72b5120a1c93b582dde86
NeurOo <orel0858@gmail.com>
2022-08-01 21:40:01 +0300

    add XlsxItemExporter

--- oknafactoria/windows_parser/settings.py
+++ oknafactoria/windows_parser/settings.py
@@ -20,3 +20,9 @@ DOWNLOAD_DELAY = 0.5
 ITEM_PIPELINES = {
    'windows_parser.pipelines.Save_to_csv': 300,
 }
+
+FEED_EXPORTERS = {
+    'xlsx': 'scrapy_xlsx.XlsxItemExporter',
+}
+
+XLSX_FILE = 'data.xlsx'
